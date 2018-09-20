ecc0d33fb31d49213ea20513ca99bd1ea07fac9e
simerc@gmail.com <profiler@vccp.com>
2018-09-20 11:18:46 +0100

    First commit for setting up webpack build

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require ('path');
+const APP_DIR = path.resolve(__dirname,'src');
+const PUBLIC_DIR = path.resolve(__dirname,'public');
+
+const config = {
+
+    entry: APP_DIR + '/HelloWorld.js',
+    devServer: {
+        contentBase: PUBLIC_DIR,
+        port: 9000,
+        open: true
+    },
+    output: {
+        path: PUBLIC_DIR,
+        filename: 'bundle.js'
+    }
+
+};
+
+module.exports = config;
+//module.exports = merge(baseConfig, config);
